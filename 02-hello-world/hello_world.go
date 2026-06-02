f4f63b2ba09cd8d3df71fd1b11a8cdef08316077
jumagu <jmgg.gutierrez@gmail.com>
2026-06-01 22:53:04 -0500

    Initial commit

---
+++ 02-hello-world/hello_world.go
@@ -0,0 +1,11 @@
+package main // Contains the code to make the file executable
+
+import "fmt" // ? import a package using import keyword
+
+func main() {
+	// Use function Println from package fmt
+	// Println is the most basic print function
+	fmt.Println("Hello World")
+
+	fmt.Printf("Hello, %s!\n", "Juan")
+}
